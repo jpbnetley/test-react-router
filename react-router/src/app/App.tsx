dffead7d89e99af19958324608c0ccfe89f3945b
Jonathan Netley <jpbnetley@gmail.com>
2025-11-03 10:05:40 +0200

    fix: react router outlet

--- react-router/src/app/App.tsx
+++ react-router/src/app/App.tsx
@@ -1,11 +1,17 @@
 
 import 'lib/styles.css';
 import './App.css';
+import { Outlet } from "react-router";
+import { MainLayout } from 'lib';
+import { TopNav } from '../components/top-nav';
 
 function App() {
 
   return (
-    <p>app</p>
+    <MainLayout topNav={<TopNav />}>
+      <p>app</p>
+      <Outlet />
+    </MainLayout>
   )
 }
 
